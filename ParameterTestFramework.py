2fed54bc9a694904143fb17b6effa2e8c5264594
TomGoh <tom-goh@outlook.com>
2021-09-24 13:58:24 +0800

    Add traditional data generator comparison

---
+++ ParameterTestFramework.py
@@ -0,0 +1,33 @@
+import numpy as np
+import tensorflow as tf
+
+from ResNet import build_ResNet18
+from dataset_utils import build_data_generator
+from SaveWeights import EarlyStoppingAtMinLoss
+
+if __name__ == '__main__':
+
+    para_dir1 = {'rotation_range': 0, 'width_shift_range': 0, 'height_shift_range': 0, 'zoom_range': 0}
+    para_dir2 = {'rotation_range': 0.15, 'width_shift_range': 0, 'height_shift_range': 0, 'zoom_range': 0}
+    para_dir3 = {'rotation_range': 0, 'width_shift_range': 0.15, 'height_shift_range': 0.15, 'zoom_range': 0}
+    para_dir4 = {'rotation_range': 0, 'width_shift_range': 0, 'height_shift_range': 0, 'zoom_range': 0.15}
+
+    para_dir = list([para_dir1, para_dir2, para_dir3, para_dir4])
+    for i, j in zip(para_dir, range(1, 5)):
+        resnet = build_ResNet18()
+        EarlyStop = EarlyStoppingAtMinLoss(patience=10)
+        resnet.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
+                       loss=tf.keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])
+        train_data, vali_data, test_data, another_test_data = build_data_generator(dataset_path='archive/', para_list=i,
+                                                                                   preprocessing_function=None,
+                                                                                   flip=False)
+        resnet_history = resnet.fit(train_data, validation_data=vali_data, epochs=50, shuffle=True,
+                                    callbacks=[EarlyStoppingAtMinLoss(patience=10)])
+        test_loss, test_accuracy  = resnet.evaluate(test_data)
+        another_test_loss, another_test_accuracy = resnet.evaluate(another_test_data)
+
+        np.savetxt('history/ValidationLoss_Generator'+str(j)+'.txt', resnet_history.history['val_loss'])
+        np.savetxt('history/ValidationAccuracy_Generator'+str(j)+'.txt', resnet_history.history['val_accuracy'])
+        np.savetxt('history/TrainingLoss_Generator'+str(j)+'.txt', resnet_history.history['loss'])
+        np.savetxt('history/TrainingAccuracy_Generator'+str(j)+'.txt', resnet_history.history['accuracy'])
+        np.savetxt('history/TestResult_Generator'+str(j)+'.txt',np.array([test_loss, test_accuracy, another_test_loss, another_test_accuracy]))
--- dataset_utils.py
+++ dataset_utils.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from PIL import Image
+import tensorflow as tf
 
 def load_folder_data(dataset_path,damaged):
     filenames = os.listdir(dataset_path)
@@ -35,3 +36,63 @@ def load_test_vali_data():
     vali_labels=np.array(vali_damage_labels+vali_no_damage_labels,dtype=np.float32)
 
     return test_images,test_labels,test_abother_images,test_another_labels,vali_images,vali_labels
+
+def build_data_generator(dataset_path,para_list, preprocessing_function=None, flip=True):
+    train_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+        rotation_range=para_list.get('rotation_range'),
+        rescale=1. / 255,
+        width_shift_range=para_list.get('width_shift_range'),
+        height_shift_range=para_list.get('height_shift_range'),
+        zoom_range=para_list.get('zoom_range'),
+        horizontal_flip=flip,
+        vertical_flip=flip,
+        preprocessing_function=preprocessing_function,
+    )
+
+    train_data = train_gen.flow_from_directory(dataset_path + 'train_another', batch_size=32, class_mode='categorical',
+                                               shuffle=True, target_size=(128, 128))
+
+    vali_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+        rotation_range=para_list.get('rotation_range'),
+        rescale=1. / 255,
+        width_shift_range=para_list.get('width_shift_range'),
+        height_shift_range=para_list.get('height_shift_range'),
+        zoom_range=para_list.get('zoom_range'),
+        horizontal_flip=flip,
+        vertical_flip=flip,
+        preprocessing_function=preprocessing_function,
+    )
+
+    vali_data = vali_gen.flow_from_directory(dataset_path + 'validation_another', batch_size=32,
+                                             class_mode='categorical', shuffle=True,
+                                             target_size=(128, 128))
+
+    test_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+        rotation_range=para_list.get('rotation_range'),
+        rescale=1. / 255,
+        width_shift_range=para_list.get('width_shift_range'),
+        height_shift_range=para_list.get('height_shift_range'),
+        zoom_range=para_list.get('zoom_range'),
+        horizontal_flip=flip,
+        vertical_flip=flip,
+        preprocessing_function=preprocessing_function,
+    )
+
+    test_data = test_gen.flow_from_directory(dataset_path + 'test', batch_size=32, class_mode='categorical',
+                                             shuffle=True,
+                                             target_size=(128, 128))
+
+    another_test_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+        rotation_range=para_list.get('rotation_range'),
+        rescale=1. / 255,
+        width_shift_range=para_list.get('width_shift_range'),
+        height_shift_range=para_list.get('height_shift_range'),
+        zoom_range=para_list.get('zoom_range'),
+        horizontal_flip=flip,
+        vertical_flip=flip,
+        preprocessing_function=preprocessing_function,
+    )
+    another_test_data = another_test_gen.flow_from_directory(dataset_path + 'test_another', batch_size=32,
+                                                             class_mode='categorical', shuffle=True,
+                                                             target_size=(128, 128))
+    return train_data, vali_data, test_data, another_test_data
